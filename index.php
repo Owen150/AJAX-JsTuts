0ca9fef2351c41b8cf86e9da5a4700c38c533736
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-24 18:44:34 +0300

    Initialized the php file with jquery

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script>
+
+</script>
+</head>
+<body>
+   <div id="comments">
+
+   </div>
+   
+   <button>Show more comments</button>
+</body>
+</html>
