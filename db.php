1de6f623c4e003c468f58eb7250498eb05d55a73
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-07-24 20:00:55 +0300

    Connecting to a mysql database using AJAX & JQuery

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "mysql";
+$username = "root";
+$password = "Xbox2022";
+$dbname = "ajax";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
--- index.php
+++ index.php
@@ -1,16 +1,37 @@
+<?php
+include 'db.php';
+?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-<script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
 
-</script>
+    </script>
 </head>
-<body>
-   <div id="comments">
 
-   </div>
-   
-   <button>Show more comments</button>
+<body>
+    <div id="comments">
+        <?php
+        $sql = "SELECT * FROM comments LIMIT 2";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<p>";
+                echo $row['author'];
+                echo "<br>";
+                echo $row['message'];
+                echo "</p>";
+            }
+        } else {
+            echo "There are no comments!";
+        }
+        ?>
+    </div>
+
+    <button>Show more comments</button>
 </body>
+
 </html>
